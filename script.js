130a28c890cd40390ef98b74fe79250800a1f560
Lautaro <forzanolautaro@gmail.com>
2024-05-25 23:09:57 -0300

    push

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const header = document.querySelector("header");
 
-    let lastScrollTop = 0;
-
-    window.addEventListener("scroll", () => {
-        let scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-        if (scrollTop > lastScrollTop) {
-            header.style.top = "-100px";
-        } else {
-            if (scrollTop === 0) {
-                header.style.top = "0";
-            }
-        }
-
-        lastScrollTop = scrollTop;
-    });
-});
